50a45f67dd1a4ef35efacad81c751d5caa44d35b
smasher44 <georgelibatique819@gmail.com>
2021-11-11 00:22:27 +0800

    adding files for Crown Clothing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/Homepage';
+
+
+const App = () => {
+  
+  return (
+    <div >
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
